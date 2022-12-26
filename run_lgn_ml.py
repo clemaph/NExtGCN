14bfd1b7ef6f515c4b4ebc11fca7b7f3e34945fa
clemaph <clemaph_z@163.com>
2022-12-26 19:03:10 +0800

    12.26 update

---
+++ run_lgn_ml.py
@@ -0,0 +1,10 @@
+from recbole.quick_start import run_recbole
+
+
+
+parameter_dict = {
+#   'neg_sampling': None,
+}
+
+config_file_list=['ml-1m.yaml']
+run_recbole(model='LightGCN', dataset='ml-1m', config_file_list=config_file_list, config_dict=parameter_dict)
